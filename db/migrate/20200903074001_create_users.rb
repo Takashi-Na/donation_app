14165d1246e41b9df922e67c8bb2ac0b2b919201
Takashi-Na <Takashi-Na@users.noreply.github.com>
2020-09-03 16:52:29 +0900

    カラム追加

--- db/migrate/20200903074001_create_users.rb
+++ db/migrate/20200903074001_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-
+      t.string :name,         nill: false
+      t.string :name_reading, nill: false
+      t.string :nickname,     nill: false
       t.timestamps
     end
   end
